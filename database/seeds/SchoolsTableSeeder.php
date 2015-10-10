6bb61bea1cb7e7b92969e137c186da1475d6cff8
Thomas Roseman <roseman.tom@gmail.com>
2015-10-10 15:33:11 -0400

    Seeder
    Added seeder for Users and Schools

---
+++ database/seeds/SchoolsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SchoolsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('schools')->insert([
+            'name' => 'Mercer Area Jr. Sr.',
+            'email' => 'test@gmail.com',
+        ]);
+
+        DB::table('schools')->insert([
+            'name' => 'Mercer Area Jr. Sr.',
+            'email' => 'somewhere@gmail.com',
+        ]);
+    }
+}
