fd7bb7be8a8817ea95ffbaa8f0be8f33f9bf50b1
Thomas Roseman <roseman.tom@gmail.com>
2016-03-14 17:28:53 -0400

    Added old tags and name values

--- resources/views/account/new.blade.php
+++ resources/views/account/new.blade.php
@@ -12,24 +12,26 @@
 
     <div class="row">
         <div class="col-lg-6 col-lg-offset-3">
-            <form method="POST" action="/account">
+            <form method="POST" action="{{ url('account') }}">
                 {{ csrf_field() }}
                 <div class="form-group">
                     <label for="accountName">Name</label>
-                    <input type="text" class="form-control" id="name" placeholder="John Dow">
+                    <input type="text" class="form-control" id="name" name="name" placeholder="John Dow" value="{{ old('name') }}">
                 </div>
                 <div class="form-group">
                     <label for="accountEmail">Email address</label>
-                    <input type="email" class="form-control" id="email" placeholder="Email">
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="{{ old('email') }}">
                 </div>
                 <div class="form-group">
                     <label for="accountPassword">Password</label>
-                    <input type="password" class="form-control" id="accountPassword" placeholder="Password">
-                    <input type="password" class="form-control" id="accountPasswordAgain" placeholder="Password">
+                    <input type="password" class="form-control" id="accountPassword" name="accountPassword" placeholder="Password">
+
+                    <label for="accountPasswordAgain">Confirm Password</label>
+                    <input type="password" class="form-control" id="accountPasswordAgain" name="accountPasswordAgain" placeholder="Password">
                 </div>
                 <div class="form-group">
                     <label for="school">School Request</label>
-                    <input  type="text" class="form-control" id="school_id" placeholder="School Id">
+                    <input  type="text" class="form-control" id="school_id" name="school_id" placeholder="School Id" value="{{ old('school_id') }}">
                 </div>
                 <button type="submit" class="col-lg-offset-3 btn btn-primary" style="height: 5em; width: 50%; border-radius: 0px">Submit</button>
             </form>
