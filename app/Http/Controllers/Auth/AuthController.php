b1ecd8fdea4a784a65a42cfba9eb5705b3c268af
Thomas Roseman <roseman.tom@gmail.com>
2016-03-15 23:27:48 -0400

    Fix for redirect and login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,6 +23,11 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
+
+    // Redirect on login
+    protected $redirectPath = '/account';
+    protected $loginPath = '/auth/login';
+
     /**
      * Create a new authentication controller instance.
      *
@@ -45,6 +50,7 @@ class AuthController extends Controller
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|confirmed|min:6',
+            'school_id' => 'required|exists:schools,id'
         ]);
     }
 
@@ -60,6 +66,7 @@ class AuthController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'school_id' => $data['school_id'],
         ]);
     }
 }
