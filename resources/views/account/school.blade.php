08db723a3be1ed7546ce1ddb3e2acd070021491c
Thomas Roseman <roseman.tom@gmail.com>
2016-03-15 23:34:54 -0400

    Update school and login view still working

--- resources/views/account/school.blade.php
+++ resources/views/account/school.blade.php
@@ -11,9 +11,6 @@
 @stop
 
 @section('content')
-    {{$user}}
-    <br>
-
     <div class="row">
         <div class="col-lg-6 col-lg-offset-3">
             <form>
@@ -26,34 +23,47 @@
                 <div class="">
                     <h3>Affiliated Accounts</h3>
                     <div class="row">
-                        <div class="col-lg-3">
+                        <div class="col-lg-6">
                             <h5>Coaches</h5>
-                            <ul>
-                                {{--@foreach($coaches as $coach)--}}
-                                    <li class="non_style_list">$coach</li>
-                                {{--@endforeach--}}
-                            </ul>
+                            <table class="table table-striped table-responsive">
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Position</th>
+                                    <th>Save</th>
+                                    <th>Remove</th>
+                                </tr>
+                                    @foreach($coaches as $coach)
+                                        <tr>
+                                            <td>{{ $coach->name }}</td>
+                                            <td>{{ $coach->name }}</td>
+                                            <td>
+                                                <select name="edit_level">
+                                                    <option value="1">Judge</option>
+                                                    <option value="2">Coach</option>
+                                                </select>
+                                            </td>
+                                            <td><button class="btn btn-primary" type="button"><span class="glyphicon glyphicon-floppy-save" aria-hidden="true"></span></button></td>
+                                            <td><button class="btn btn-danger" type="button"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></td>
+                                        </tr>
+                                    @endforeach
+                            </table>
                         </div>
-                        <div class="col-lg-3">
+                        <div class="row">
+                        <div class="col-lg-6">
                             <h5>Judges</h5>
                             <ul>
-                                {{--@foreach($judges as $judge)--}}
-                                    <li class="non_style_list">$judge</li>
-                                {{--@endforeach--}}
+                                @foreach($judges as $judge)
+                                    <li class="non_style_list"><span>{{ $judge->name}}</span> <span>{{$judge->edit_level }}</span></li>
+                                @endforeach
                             </ul>
+                        </div>
                         </div>
                     </div>
 
                 </div>
 
-                <div class="form-group">
-                    @foreach($emails as $email)
 
-                        <label for="schoolName">Emails</label>
-                        <input type="text" class="form-control" id="email[]" placeholder="someone@someplace.com"
-                               value="{{$email->email}}">
-                    @endforeach
-                </div>
             </form>
         </div>
     </div>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
     <div class="row">
         <div class="col-lg-6 col-lg-offset-3">
 
-            <form method="POST" action="{!!url('auth/logint') !!}">
+            <form method="POST" action="{!!url('auth/login') !!}">
                 {!!csrf_field()!!}
 
                 <div>
